f76866b5d96bca06f5023af7583333cd32b708a9
Sara Ferreira <sara2004.souza@gmail.com>
2026-05-12 01:57:27 -0300

    feat: add landing page components including Navbar and HeroSection

---
+++ src/app/landingPage/page.tsx
@@ -0,0 +1,11 @@
+import { HeroSection } from '@/components/landing/HeroSection';
+import { Navbar } from '@/components/landing/Navbar';
+
+export default function HomePage() {
+  return (
+    <>
+        <Navbar />
+        <HeroSection />
+    </>
+  );
+}

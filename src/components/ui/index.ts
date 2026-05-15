dc0f4d71881512c270e3fc01c755a0058ce1019f
Kauan Farias <130491230+kakafariaZ@users.noreply.github.com>
2026-05-15 16:14:28 -0300

    refactor: move Searchbar to ui and update HomePage import (#43)
    Co-authored-by: Kauan Farias <kauanfarias@MacBook-Pro-de-Kauan.local>

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,4 +2,5 @@ export * from './inputs';
 export { Button } from './button';
 export { default as Header } from './Header';
 export { default as Sidebar } from './Sidebar';
+export { default as Searchbar } from './Searchbar';
 export type { NavLink } from './Sidebar';

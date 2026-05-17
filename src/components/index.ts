7853705a64543ea6ed1bac29f5a304b868f5df2d
Sara Ferreira <143294885+sarinhasf@users.noreply.github.com>
2026-05-16 21:59:49 -0300

    feat: implement EventCreatedPage with sample events and layout (#53)

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ export * from './modals';
 export * from './LabelChip';
 export * from './ScheduleCard';
 export * from './ActivityDetail';
+export * from './EventsList';
 export { EventCard } from './EventCard';
 export { default as ActivityList } from './activityList';

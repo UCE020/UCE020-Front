642874850eb6c489ddcf5b9f8eaae5e081b1bf80
laizagordiano <laizagordiano.5@gmail.com>
2026-05-18 15:22:07 -0300

    feat: add tela de certificados e componete de certificado

---
+++ src/types/certificate.ts
@@ -0,0 +1,11 @@
+export interface Certificate {
+  id: string;
+  title: string;
+  participantName: string;
+  hours: number;
+  imageUrl?: string;
+  issueDate: string;
+  createdAt: string;
+  updatedAt: string;
+}
+

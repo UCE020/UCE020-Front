4f15b040ab984ce9ebada8b71f0f08b41c497f94
brendatrindade <brendatrindde@gmail.com>
2026-05-26 01:08:37 -0300

    feat: refactor ActivityModal to use ActivityModalActions component and update font size in ScheduleDetails

--- src/components/modals/ActivityModal/ActivityModal.tsx
+++ src/components/modals/ActivityModal/ActivityModal.tsx
@@ -2,9 +2,8 @@
 
 import { ModalContainer, CloseButton } from '@/components/modals';
 import { ScheduleCard } from '@/components';
-import { Button } from '@/components/ui';
 import type { ActivityModalProps } from '@/types/activity';
-import { Box } from '@mui/material';
+import { ActivityModalActions } from './ActivityModalActions';
 
 export default function ActivityModal({
   open,
@@ -22,6 +21,8 @@ export default function ActivityModal({
   onSignup,
   onCancelParticipation,
   onMarkPresence,
+  onValidatePresences,
+  onListParticipants,
 }: ActivityModalProps) {
   return (
     <ModalContainer open={open} onClose={onClose}>
@@ -39,59 +40,14 @@ export default function ActivityModal({
         description={description}
       />
 
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'center',
-          gap: 1,
-          width: '100%',
-          mt: 'auto',
-        }}
-      >
-        {variant === 'signup' ? (
-          <Button
-            variant="contained"
-            color="secondary"
-            onClick={onSignup}
-            fullWidth
-            sx={{
-              height: 36,
-              fontSize: 'clamp(10px, 3vw, 12px)',
-              maxWidth: 160,
-            }}
-          >
-            Inscrever-se
-          </Button>
-        ) : (
-          <>
-            <Button
-              variant="outlined"
-              color="secondary"
-              onClick={onCancelParticipation}
-              sx={{
-                height: 36,
-                fontSize: 'clamp(10px, 3vw, 12px)',
-                flex: 1,
-              }}
-            >
-              Cancelar participação
-            </Button>
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={onMarkPresence}
-              sx={{
-                height: 36,
-                fontSize: 'clamp(10px, 3vw, 12px)',
-                flex: 1,
-              }}
-            >
-              Marcar Presença
-            </Button>
-          </>
-        )}
-      </Box>
+      <ActivityModalActions
+        variant={variant}
+        onSignup={onSignup}
+        onCancelParticipation={onCancelParticipation}
+        onMarkPresence={onMarkPresence}
+        onValidatePresences={onValidatePresences}
+        onListParticipants={onListParticipants}
+      />
     </ModalContainer>
   );
 }

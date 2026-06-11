7564c5669eb665af13b4e75db3533fdc81850a20
LuisMarioRC <luismariocarneiro8@gmail.com>
2026-06-10 22:38:45 -0300

    feat: atualizando o dto para o retorno da rota

--- src/types/event.ts
+++ src/types/event.ts
@@ -1,13 +1,20 @@
-export interface Event {
-  id: string;
-  name: string;
-  startDate: string;
-  endDate: string;
-  time: string;
-  imageUrl: string;
+export interface IEvent {
+  id: number;
+  nome: string;
+  codigo: string;
+  descricao: string;
+  localizacao: string;
+  responsavel: string;
+  cargaHoraria: number;
+  dataInicio: string;
+  dataFim: string;
+  status: string;
+  foto: string | null;
+  createdAt: string;
+  updatedAt: string;
 }
 
 export interface EventCardProps {
-  event: Event;
-  onClick?: (event: Event) => void;
+  event: IEvent;
+  onClick?: (event: IEvent) => void;
 }
